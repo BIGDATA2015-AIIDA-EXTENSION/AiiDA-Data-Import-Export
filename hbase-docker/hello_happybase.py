4af4c8a042f1973442cfec550b08ad7fbe0bf885
Artur Skonecki <skoneka@users.noreply.github.com>
2015-03-21 12:57:57 +0100

    hbase-docker

---
+++ hbase-docker/hello_happybase.py
@@ -0,0 +1,8 @@
+import happybase
+connection = happybase.Connection('hbase-docker', 9090)
+connection.create_table('table-name', { 'family': dict() } )
+connection.tables()
+table = connection.table('table-name')
+table.put('row-key', {'family:qual1': 'value1', 'family:qual2': 'value2'})
+for k, data in table.scan():
+  print k, data
