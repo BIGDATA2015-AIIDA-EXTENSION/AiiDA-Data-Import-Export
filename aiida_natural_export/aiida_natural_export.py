3d340d49d18089f6b72309d1c647d873974b630d
Artur Skonecki <skoneka@users.noreply.github.com>
2015-04-26 02:22:31 +0200

    coments

--- aiida_natural_export/aiida_natural_export.py
+++ aiida_natural_export/aiida_natural_export.py
@@ -1,5 +1,22 @@
 # paste it into 'verdi shell'
 
+"""
+giovannipizzi [10:13 AM]
+You can loop over structure.sites
+And for each site store the kind_name and the coordinates (3 floats)
+If you want to store everything, in case of doubt, both s
+Each kind object and each site object (the elements returned by S.kinds and s.sites
+Should have a get_raw() method that gives you a storable dict with all the stored info
+Which basically is kind_name and positions for the sites
+And some info for the kinds, that you won't probably use for the project, but it's good to have in generale
+Then it's correct that you store the cell
+And if you want to be complete, you can also store structure.pbc, an array of 3 booleans
+Which in the data provided should always be [T,T,T]
+Pbc= periodic boundary conditions
+It says if the system is periodic along the 1st,2nd and 3rd axis
+"""
+
+
 import aiida
 import aiida.orm
 import aiida.orm.data
